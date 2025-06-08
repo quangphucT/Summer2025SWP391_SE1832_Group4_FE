3779eff645f15acb63d852068b76e14698cdad60
unknown <phucp9698@gmail.com>
2025-06-08 23:43:48 +0700

    handle logic change password, fix ui update profile, set up modal dynamic in atoms folder

--- src/apis/index.js
+++ src/apis/index.js
@@ -3,3 +3,5 @@ export * as registerAPI from "./authenticationApi/registerApi";
 export * as setupPassworApi from "./authenticationApi/setupPasswordApi"
 export * as forgotPasswordApi from "./authenticationApi/forgotPasswordApi"
 export * as resetPasswordApi from "./authenticationApi/resetPasswordApi"
+export * as updateProfileApi from "./authenticationApi/updateProfileApi"
+export * as changePasswordApi from "./authenticationApi/changePasswordApi"
