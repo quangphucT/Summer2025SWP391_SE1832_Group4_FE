bbd8ac8f7488729a5822da2adcce85e60beda025
unknown <phucp9698@gmail.com>
2025-06-13 07:39:44 +0700

    handle logic booking consultation

--- src/pages/schedule-consultation-page/index.jsx
+++ src/pages/schedule-consultation-page/index.jsx
@@ -9,48 +9,74 @@ import {
   Form,
   Col,
   Row,
-} from 'antd';
-import { useState } from 'react';
-import dayjs from 'dayjs';
-import customParseFormat from 'dayjs/plugin/customParseFormat';
-import 'dayjs/locale/vi';
-import Symstom from '../../components/atoms/Symstom';
-import { motion } from 'framer-motion';
+} from "antd";
+import { useEffect, useState } from "react";
+import dayjs from "dayjs";
+import customParseFormat from "dayjs/plugin/customParseFormat";
+import "dayjs/locale/vi";
+import Symstom from "../../components/atoms/Symstom";
+import { motion } from "framer-motion";
+import { toast } from "react-toastify";
+import { getAllDoctorsApi } from "../../apis/doctorApi/getAllDoctorsApi";
 
 dayjs.extend(customParseFormat);
 
 const ScheduleAConsultation = () => {
   const [form] = Form.useForm();
-  const [selectedType, setSelectedType] = useState('online-video');
+  const [selectedType, setSelectedType] = useState("online-video");
+  const [dataDoctors, setDataDoctors] = useState([]);
 
   const onFinish = (values) => {
-    console.log('Form values:', values);
-    // Gửi API ở đây
-  };
+    const formattedDate = values.appointmentDate.format("YYYY-MM-DD");
+    const formattedTime = values.appointmentTime.format("HH:mm:ss");
+    const goodValues = {
+ ...values,
+      appointmentDate: formattedDate,
+      appointmentTime: formattedTime
+    }
+     
+    
+    console.log("📅 Ngày hẹn:", formattedDate);
+    console.log("🕒 Giờ hẹn:", formattedTime);
+    console.log("✅ Tất cả giá trị form:", goodValues);
 
+    // Hiển thị ra UI nếu muốn (ví dụ cập nhật state để show bên ngoài)
+  };
+  const fetchingAllDataDoctors = async () => {
+    try {
+      const response = await getAllDoctorsApi();
+      setDataDoctors(response.data.data);
+    } catch (error) {
+      toast.error(
+        error?.response?.data?.message || "Error while fetching data!"
+      );
+    }
+  };
+  useEffect(() => {
+    fetchingAllDataDoctors();
+  }, []);
   return (
     <div className="min-h-screen bg-[#e0e7ff] p-6 mt-[75px]">
       <Row gutter={24} justify="center">
         {/* Bên trái: Symptom */}
         <Col xs={24} md={8}>
-           <motion.div
-      initial={{ opacity: 0, x: -50 }}
-      animate={{ opacity: 1, x: 0 }}
-      transition={{ duration: 0.8, delay: 0.2 }}
-    >
-      <Symstom />
-    </motion.div>
+          <motion.div
+            initial={{ opacity: 0, x: -50 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.8, delay: 0.2 }}
+          >
+            <Symstom />
+          </motion.div>
         </Col>
 
         {/* Bên phải: Form */}
         <Col xs={24} md={16}>
-        
           <motion.div
-      initial={{ opacity: 0, x: 50 }}
-      animate={{ opacity: 1, x: 0 }}
-      transition={{ duration: 0.8, delay: 0.4 }}
-      className="bg-white rounded-4xl shadow-2xl p-8 max-h-[90vh] overflow-y-auto"
-    >
+            initial={{ opacity: 0, x: 50 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.8, delay: 0.4 }}
+            className="bg-white rounded-4xl shadow-2xl p-8 max-h-[90vh] overflow-y-auto"
+          >
             <h2 className="text-3xl font-bold text-[#1e88e5] mb-8 text-center">
               🗓️ Schedule A Consultation
             </h2>
@@ -60,26 +86,23 @@ const ScheduleAConsultation = () => {
               name="scheduleConsultation"
               onFinish={onFinish}
               layout="vertical"
-              initialValues={{
-                appointmentDate: dayjs(),
-                appointmentTime: dayjs('14:00', 'HH:mm'),
-                consultationType: 'online-video',
-                isAnonymous: false,
-              }}
             >
               <Row gutter={16}>
                 {/* Cột trái */}
                 <Col xs={24} md={12}>
+
+                 
+
                   <Form.Item
                     name="appointmentDate"
                     label="Choose Date"
-                    rules={[{ required: true, message: 'Date is required!' }]}
+                    rules={[{ required: true, message: "Date is required!" }]}
                   >
                     <DatePicker
                       className="w-full h-[45px]"
                       format="YYYY-MM-DD"
                       disabledDate={(current) =>
-                        current && current < dayjs().startOf('day')
+                        current && current < dayjs().startOf("day")
                       }
                     />
                   </Form.Item>
@@ -87,7 +110,7 @@ const ScheduleAConsultation = () => {
                   <Form.Item
                     name="appointmentTime"
                     label="Choose Hour (30 minutes)"
-                    rules={[{ required: true, message: 'Hour is required!' }]}
+                    rules={[{ required: true, message: "Hour is required!" }]}
                   >
                     <TimePicker
                       className="w-full h-[45px]"
@@ -97,22 +120,22 @@ const ScheduleAConsultation = () => {
                   </Form.Item>
 
                   <Form.Item
-                    name="reason"
+                    name="appointmentService"
                     label="Choose Consultation Type"
-                    rules={[{ required: true, message: 'Type is required!' }]}
+                    rules={[{ required: true, message: "Type is required!" }]}
                   >
                     <Select
                       className="w-full !h-[45px]"
                       placeholder="Choose a consultation"
                       options={[
-                        { value: 'test-hiv', label: 'HIV Testing' },
+                        { value: "HIVTesting", label: "HIV Testing" },
                         {
-                          value: 'psychology',
-                          label: 'Mental Health / Psychological Support',
+                          value: "MentalHealth",
+                          label: "Mental Health / Psychological Support",
                         },
                         {
-                          value: 'new-diagnosis',
-                          label: 'Newly Diagnosed with HIV',
+                          value: "NewlyDiagnosedHIV",
+                          label: "Newly Diagnosed with HIV",
                         },
                       ]}
                     />
@@ -121,11 +144,34 @@ const ScheduleAConsultation = () => {
 
                 {/* Cột phải */}
                 <Col xs={24} md={12}>
+                   
+                    <Form.Item
+                    name="doctorId"
+                    label="Choose Doctor"
+                    rules={[
+                      { required: true, message: "Please choose a doctor!" },
+                    ]}
+                  >
+                    <Select
+                      className="w-full !h-[45px]"
+                      placeholder="Select a doctor"
+                      showSearch
+                      optionFilterProp="label"
+                      options={dataDoctors.map((doctor) => ({
+                        label: `${doctor.fullName} (${doctor.email})`,
+                        value: doctor.doctorId,
+                      }))}
+                    />
+                  </Form.Item>
+
                   <Form.Item
-                    name="consultationType"
+                    name="appointmentType"
                     label="Consultation Method"
                     rules={[
-                      { required: true, message: 'Consultation method required' },
+                      {
+                        required: true,
+                        message: "Consultation method required",
+                      },
                     ]}
                   >
                     <Radio.Group
@@ -133,29 +179,31 @@ const ScheduleAConsultation = () => {
                       value={selectedType}
                       onChange={(e) => setSelectedType(e.target.value)}
                     >
-                      <Radio.Button value="online-video">Video</Radio.Button>
-                      <Radio.Button value="online-phone">Phone</Radio.Button>
-                      <Radio.Button value="online-chat">Chat</Radio.Button>
-                      <Radio.Button value="offline">In-person</Radio.Button>
+                      <Radio.Button value="Video">Video</Radio.Button>
+                      <Radio.Button value="phone">Phone</Radio.Button>
+                      <Radio.Button value="Chat">Chat</Radio.Button>
+                      <Radio.Button value="InPerson">In Person</Radio.Button>
                     </Radio.Group>
                   </Form.Item>
 
-                  {selectedType === 'offline' && (
+                  {/* {selectedType === "InPerson" && (
                     <Form.Item
                       name="location"
                       label="Location"
-                      rules={[{ required: true, message: 'Location is required!' }]}
+                      rules={[
+                        { required: true, message: "Location is required!" },
+                      ]}
                     >
                       <Select
                         className="w-full !h-[45px]"
                         placeholder="Select location"
                         options={[
-                          { value: 'hanoi', label: 'Hà Nội - 123 Đường ABC' },
-                          { value: 'hcm', label: 'TP.HCM - 456 Đường XYZ' },
+                          { value: "hanoi", label: "Hà Nội - 123 Đường ABC" },
+                          { value: "hcm", label: "TP.HCM - 456 Đường XYZ" },
                         ]}
                       />
                     </Form.Item>
-                  )}
+                  )} */}
 
                   <Form.Item name="isAnonymous" valuePropName="checked">
                     <Checkbox>Is Anonymous Consultation</Checkbox>
@@ -164,8 +212,11 @@ const ScheduleAConsultation = () => {
 
                 {/* Ghi chú */}
                 <Col span={24}>
-                  <Form.Item name="notes" label="Notes">
-                    <Input.TextArea rows={3} placeholder="Ghi chú thêm (nếu có)" />
+                  <Form.Item name="appointmentNotes" label="Notes">
+                    <Input.TextArea
+                      rows={3}
+                      placeholder="Taking notes (if having)"
+                    />
                   </Form.Item>
                 </Col>
 
@@ -177,14 +228,13 @@ const ScheduleAConsultation = () => {
                       htmlType="submit"
                       className="w-full !h-[47px] !font-bold !rounded-4xl"
                     >
-                     Booking Now
+                      Booking Now
                     </Button>
                   </Form.Item>
                 </Col>
               </Row>
             </Form>
-        </motion.div>
-
+          </motion.div>
         </Col>
       </Row>
     </div>
